4430ff22cb7e8cccc449518f851c1f2a25e974b1
Sai Ashish <33771241+saiashish90@users.noreply.github.com>
2021-05-11 14:19:08 +0530

    name is now centered

--- pages/index.js
+++ pages/index.js
@@ -54,11 +54,13 @@ export default function index() {
         {/* Text */}
         <div className={Home.text}>
           {/* Name */}
-          <h1 className={Home.h1}>Hi, I'm Sai Ashish</h1>
-          {/* Designation */}
-          <div className={Home.desig}>
-            <h2>A Computer Science Engineer</h2>
+          <div className={Home.name}>
+            <h1 className={Home.h1}>Hi, I'm Sai Ashish</h1>
+            <div className={Home.desig}>
+              <h2>A Computer Science Engineer</h2>
+            </div>
           </div>
+          {/* Designation */}
         </div>
         <div className={Home.projects}>
           <span>Projects</span>
